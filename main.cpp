7c3ba3b8104af7dc4468a91a477ae662fa6e9157
wcx <bethaniebell8474@gmail.com>
2021-10-26 14:48:22 +0800

    update README.md

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 int main() {
     clock_t a = clock();
-    cout<<BigInteger(999).pow(99).excel_sqrt()<<endl;
+//    cout<<BigInteger(999).pow(99).excel_sqrt()<<endl;
+//    BigInteger x = BigInteger::factorial(1000);
+    for(int i = 0;i< 10;i++){
+        cout<<BigInteger::factorial(10000)<<endl;
+    }
+//    cout<<x.excel_sqrt()<<endl;
     clock_t b = clock();
     cout<<b-a<<" ms"<<endl;
     return 0;
